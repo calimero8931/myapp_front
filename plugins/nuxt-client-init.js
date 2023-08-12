ba61541797e44f2f74f1fbf3fcbde869341504ac
Yuya Kozuma <hello.potechi@gmail.com>
2023-08-12 23:36:24 +0900

    Add plugins for stay logged in

---
+++ plugins/nuxt-client-init.js
@@ -0,0 +1,8 @@
+// ログインの維持
+export default async ({ $auth, $axios }) => {
+  await $axios.$post(
+    '/api/v1/auth_token/refresh',
+    {},
+    { validateStatus: status => $auth.resolveUnauthorized(status) }
+  ).then(response => $auth.login(response))
+}
